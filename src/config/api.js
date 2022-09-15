3a7cb27c498323aef26b1196dd35105f4243f3fb
Ade Fitriana <adefitriana007@gmail.com>
2022-09-15 10:41:30 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL: process.env.REACT_APP_BASEURL,
 });
 
 export const setAuthToken = (token) => {
