8d895966bdfeb5e2e8b879d433a8a69768c8cd52
Ade Fitriana <adefitriana007@gmail.com>
2022-09-17 10:58:17 +0700

    update socket

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: process.env.REACT_APP_BASEURL,
+  baseURL: process.env.REACT_APP_BASEURL + process.env.REACT_APP_API_VERSION,
 });
 
 export const setAuthToken = (token) => {
